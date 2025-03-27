933cc06db324f2ceffa24e26ae482b0fe2b0cff7
Gabriel Pereira <gabpereira2005@gmail.com>
2025-03-27 20:33:18 -0300

    mensagem

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 function calcular(){
     // vamos criar 2 variaveis
     // Js as variaveis nao possuem tipo
-    let mascote, homenagem
+    let mascote, homenagem, leite, kitAvulso, suplementoAvulso
     // recupera o valor do mascote digitado pelo usuario
     mascote = document.getElementById("mascote").value
-    alert("mascote")
     // recupera o valor da homenagem digitado pelo usuario
     homenagem = document.getElementById("homenagem").value
-    alert("homenagem")
-    // calcular a some
-    let soma = Number(mascote) + Number(homenagem)
-    alert(soma)
+    // recupera a quanidade de  litros de leite
+    leite = document.getElementById("leite").value
+    // recuperar a quantidade avulsa de kits de alimentação
+    kitAvulso = document.getElementById("kitAvulso").value
+    // recuperar a quantidade avulsa de suplementos
+    suplementoAvulso = document.getElementById("suplementoAvulso").value
+    // calcular a soma
+    let soma = Number(mascote) + Number(homenagem) + (2*Number(leite)) + (30*Number(kitAvulso)) + (15*Number(suplementoAvulso))
+    //retorna o valor ao HTML
+    document.getElementById("soma").innerHTML=`A soma é ${soma}`
 }
