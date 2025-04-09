056684bacbb69041b40125af20bfa5f7a4afa3c8
Gabriel Pereira <gabpereira2005@gmail.com>
2025-04-08 22:13:22 -0300

    soma

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function calcular(){
     // vamos criar 2 variaveis
     // Js as variaveis nao possuem tipo
-    let mascote, homenagem, leite, kit, suplemento, soma, equipe
+    let mascote, homenagem, leite, kit, suplemento, soma, equipe, sangue
     // recupera o valor do mascote digitado pelo usuario
     mascote = Number(document.getElementById("mascote").value)
     // recupera o valor da homenagem digitado pelo usuario
@@ -16,19 +16,43 @@ function calcular(){
     soma = (mascote) + (homenagem) + (2*(leite))
     //vamos calcular a pontuacao considerando metas de kit e supl.
     equipe = document.getElementById("equipe").value
+    //doacao de sangue
+    sangue = Number(document.getElementById("sangue").value)
     if (equipe == "laranja"){
-        if (kit>=97){
-            soma = soma + 5000 + ((kit-97) * 30)
+        if (kit >= 97 && suplemento >= 49) {
+            soma = soma + 5000 + ((kit-97) * 30) + ((suplemento - 49) * 15)
         }
-        else if (kit>=78){
-            soma = soma + 4000 + ((kit-78) * 30)
+        else if (kit >= 78 && suplemento >= 39){
+            soma = soma + 4000 + ((kit-78) * 30) + ((suplemento - 39) * 15)
         }
-        else if (kit>=49){
-            soma = soma + 2500 ((kit-49) * 30)
+        else if (kit >= 49 && suplemento >= 25){
+            soma = soma + 2500 + ((kit-49) * 30) + ((suplemento - 25) * 15)
         }
-        else if (kit>=19){
-            soma = soma + 1000 ((kit-19) * 30)
+        else if (kit >= 19 && suplemento >= 10){
+            soma = soma + 1000 + ((kit-19) * 30) + ((suplemento - 10)* 15)
         }
+        if (sangue >=49){
+            soma = soma + 2500 + ((sangue - 49) * 20)
+        }
+        else {
+            soma = soma + (sangue * 20)
+        }
+    }
+    else if (equipe == "preta"){
+            //verifica kit e suplemento
+            //verifica sangue
+            if ( sangue >= 52) {
+                soma = soma +
+            }
+    }
+    else if (equipe == "roxa"){
+
+    }
+    else if (equipe == "verde"){
+
+    }
+    else {// equipe vermelha
+
     }
     //retorna o valor ao HTML
     //template string
